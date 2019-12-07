a072a8e6f85306b49b49df4d5825344e314a345a
ngovanphat <1753082@student.hcmus.edu.vn>
2019-12-07 17:59:58 +0700

    Add 404 Page

--- app.js
+++ app.js
@@ -17,6 +17,11 @@ app.get('/', function(req, res) {
 app.get('/login', function(req, res) {
     res.render('login');
 });
+app.use(function(req, res) {
+    res.render('404', {
+        layout: false
+    });
+})
 const PORT = 3000;
 app.listen(PORT, function() {
     console.log(`Server is running at http://localhost:${PORT}`);
