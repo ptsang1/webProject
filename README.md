00b1fc140f8786daac851129e0dc97b083386ec0
Quan Le <nauqmac@Nauq-Macbook-Pro.local>
2019-12-08 11:08:48 +0700

    MQ 1 commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# WEB PROJECT
+Class: Web programming
+Member: Phung Thanh Sang, Ngo Van Phat, Truong Nhat Minh, Le Minh Quan
---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+
+var app = express();
+
+app.use(express.static('public'))
+
+app.engine('hbs', exphbs({
+    defaultLayout: 'main.hbs'
+}));
+
+app.set('view engine', 'hbs');
+
+app.get('/', function(req, res) {
+    res.render('home');
+});
+app.get('/login', function(req, res) {
+    res.render('login');
+});
+app.use(function(req, res) {
+    res.render('404', {
+        layout: false
+    });
+})
+const PORT = 3000;
+app.listen(PORT, function() {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
