dcc7510ec80c4845eb017a67279d83f9d33fd929
ngovanphat <1753082@student.hcmus.edu.vn>
2020-01-06 22:30:24 +0700

    change add product

--- routes/_product.route.js
+++ routes/_product.route.js
@@ -3,10 +3,13 @@ const categoryModel = require('../models/category.model');
 const productModel = require('../models/product.model');
 const multer = require('multer');
 const moment = require('moment');
-const upload = multer({ dest: 'upload/' });
+
 const config = require('../config/default.json');
 const router = express.Router();
+
+
 router.use(express.static('public'));
+
 
 router.get('/add', async function(req, res) {
     result = await categoryModel.all();
@@ -15,27 +18,42 @@ router.get('/add', async function(req, res) {
     });
 })
 
-router.post('/add', upload.array('imageProduct', 3), async function(req, res) {
-    let tn = new Date();
-    let catID = await categoryModel.singleByName(req.body.category);
-    console.log(req.body);
-    const entity = {
-        sellerID: 'SellerVipPro',
-        CatID: catID.catID,
-        productName: req.body.productName,
-        bidderID: '',
-        priceCurent: req.body.priceCurrent,
-        stepPrice: req.body.stepPrice,
-        price: req.body.price,
-        sold: 0,
-        timePost: tn,
-        timeEnd: tn
-    };
-    const rs = await productModel.add(entity);
-    result = await categoryModel.all();
-    res.render('vwProduct/add', {
-        categories: result
+router.post('/add', async function(req, res) {
+    const storage = multer.diskStorage({
+        filename: function(req, file, cb) {
+            cb(null, file.originalname)
+        },
+        destination: function(req, file, cb) {
+            cb(null, `./public/images/`);
+        },
     });
+    const upload = multer({ storage });
+    upload.array('input-b1', 3)(req, res, async function(err) {
+        if (err) {
+
+        } else {
+            let tn = new Date();
+            let catID = await categoryModel.singleByName(req.body.category);
+            console.log(req.body);
+            const entity = {
+                sellerID: 'SellerVipPro',
+                CatID: catID.catID,
+                productName: req.body.productName,
+                bidderID: '',
+                priceCurent: req.body.priceCurrent,
+                stepPrice: req.body.stepPrice,
+                price: req.body.price,
+                sold: 0,
+                timePost: tn,
+                timeEnd: tn
+            };
+            const rs = await productModel.add(entity);
+            result = await categoryModel.all();
+            res.render('vwProduct/add', {
+                categories: result
+            });
+        }
+    })
 })
 
 router.get('/detail', async function(req, res) {
