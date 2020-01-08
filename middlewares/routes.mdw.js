aa5fd67bb03d5d6dfade4eb027c6a827a6619379
ngovanphat <1753082@student.hcmus.edu.vn>
2020-01-08 12:32:00 +0700

    authentication for admin and add product

--- middlewares/routes.mdw.js
+++ middlewares/routes.mdw.js
@@ -5,5 +5,6 @@ module.exports = function(app) {
     app.use('/product', require('../routes/_product.route'));
     app.use('/account', require('../routes/account.route'));
     app.use('/profile', require('../routes/profile.route'));
+    app.use('/admin', require('../routes/admin.route'));
     app.use('/auth', require('../routes/auth.route'));
 };
